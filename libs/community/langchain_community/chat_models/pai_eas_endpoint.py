a1753ce0fe8b14dcffbeae728316046027254bac
jinyulin1 <jinyulin1@qq.com>
2024-08-20 13:59:29 +0800

    Fix Issue #20453: Change _call_eas to return bytes instead of str

--- libs/community/langchain_community/chat_models/pai_eas_endpoint.py
+++ libs/community/langchain_community/chat_models/pai_eas_endpoint.py
@@ -215,19 +215,15 @@ class PaiEasChatEndpoint(BaseChatModel):
             "Accept": "application/json",
             "Authorization": f"{self.eas_service_token}",
         }
-
         # make request
         response = requests.post(
             self.eas_service_url, headers=headers, json=query_body, timeout=self.timeout
         )
-
         if response.status_code != 200:
             raise Exception(
-                f"Request failed with status code {response.status_code}"
-                f" and message {response.text}"
+                f"Request failed with status code {response.status_code}" f" and message {response.text}"
             )
-
-        return response.text
+        return response.content
 
     def _call_eas_stream(self, query_body: dict) -> Any:
         """Generate text from the eas service."""
